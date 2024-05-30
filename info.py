cef1583c9e2bc7c9168b1ceecca09a3cd14a00f9
saimshamim8423 <144903153+saimshamim8423@users.noreply.github.com>
2024-05-30 11:14:44 +0530

    Update info.py

--- info.py
+++ info.py
@@ -17,7 +17,7 @@ def is_enabled(value, default):
         return default
 
 # Bot information
-SESSION = environ.get('SESSION', 'TechSmilebotz')
+SESSION = environ.get('SESSION', 'TechVJBot')
 API_ID = int(environ.get('API_ID', ''))
 API_HASH = environ.get('API_HASH', '')
 BOT_TOKEN = environ.get('BOT_TOKEN', "")
